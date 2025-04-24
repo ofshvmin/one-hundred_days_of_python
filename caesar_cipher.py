f77430ba4da73945ca342404e1598f46ecdbea65
Daniel Mathews <daniel.g.mathews@gmail.com>
2025-04-24 11:40:15 -0400

    add decode functionality. add option to loop back to beginning.

--- caesar_cipher.py
+++ caesar_cipher.py
@@ -1,21 +1,34 @@
-from operator import indexOf
+import art
+print(art.logo)
 
 alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
 
-direction = input("Type 'encode' to encrypt, type 'decode' to decrypt:\n").lower()
-text = input("Type your message:\n").lower()
-shift = int(input("Type the shift number:\n"))
+def caesar(original_text, shift_amount, encode_or_decode):
+    output_text = ""
+    if encode_or_decode == "decode":
+        shift_amount *= -1
 
-
-def encrypt(original_text, shift_amount):
-    new_text = ""
     for letter in original_text:
-        if indexOf(alphabet, letter) + shift_amount <= 25:
-            # find the index, add shift_amount, look up new letter, concat in new
-            new_text += alphabet[indexOf(alphabet, letter) + shift_amount]
+
+        if letter not in alphabet:
+            output_text += letter
         else:
-            new_text += alphabet[indexOf(alphabet, letter) % len(alphabet)]
 
-    print(f"Here is the encoded text: {new_text}")
+            shifted_position = alphabet.index(letter) + shift_amount
+            shifted_position %= len(alphabet)
+            output_text += alphabet[shifted_position]
+    print(f"Here is the {encode_or_decode}d result: {output_text}")
 
-encrypt(original_text=text, shift_amount=shift)
+should_continue = True
+
+while should_continue:
+    direction = input("Type 'encode' to encrypt, type 'decode' to decrypt:\n").lower()
+    text = input("Type your message:\n").lower()
+    shift = int(input("Type the shift number:\n"))
+
+    caesar(original_text=text, shift_amount=shift, encode_or_decode=direction)
+
+    restart = input("Type 'yes' if you want ot go again.  Otherwise, type 'no'.\n").lower()
+    if restart == "no":
+        should_continue = False
+        print("Goodbye")
